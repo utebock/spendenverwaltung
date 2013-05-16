a7ce0ea14727c9424433ff250806acc9848179e4
Philipp Muhoray <e0967964@student.tuwien.ac.at>
2013-05-17 00:15:06 +0200

    @Override for AddressDAOImplemented.java

--- src/main/java/dao/AddressDAOImplemented.java
+++ src/main/java/dao/AddressDAOImplemented.java
@@ -12,27 +12,32 @@ import exceptions.PersistenceException;
  * 
  */
 public class AddressDAOImplemented implements IAddressDAO {
-	
+
+	@Override
 	public Address create(Address a) throws PersistenceException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public Address update(Address a) throws PersistenceException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public void delete(Address a) throws PersistenceException {
 		// TODO Auto-generated method stub
 
 	}
 
+	@Override
 	public List<Address> getAll() throws PersistenceException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public Address getByID(int id) throws PersistenceException {
 		// TODO Auto-generated method stub
 		return null;
