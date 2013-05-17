0f745eb345b34ce1461fa29efc77b57d2acf574a
Chris Steele <0708104@gmail.com>
2013-05-17 18:21:24 +0200

    Fixed spring config, added sql connector to pom.xml, implemented datasource initialization test

---
+++ src/test/java/dao/DataSourceTest.java
@@ -0,0 +1,28 @@
+package dao;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+public class DataSourceTest {
+
+	/**
+	 * this is a primitive test that ensures that Spring is able to connect to the database
+	 * and that the TransactionManager is constructed correctly. if no exceptions are thrown,
+	 * the initializations should have succeeded. this is not meant to be a true test, but rather
+	 * a quick check to see that the spring config seems to be working...
+	 */
+	
+	@Test
+	public void testConnectionShouldNotThrowException() {
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("testspring.xml");
+		
+		@SuppressWarnings("unused")
+		DataSourceTransactionManager transactionManager = context.getBean("transactionManager", DataSourceTransactionManager.class);
+		
+		assert(true);
+	}
+
+}
