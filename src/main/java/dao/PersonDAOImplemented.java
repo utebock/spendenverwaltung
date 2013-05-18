bda4ec29ab4c37bf4efcec1f05220a29bfe255cf
Chris Steele <0708104@gmail.com>
2013-05-18 23:49:36 +0200

    Removed AddressDAO from PersonDAO, no dependency is required...

--- src/main/java/dao/PersonDAOImplemented.java
+++ src/main/java/dao/PersonDAOImplemented.java
@@ -6,12 +6,6 @@ import domain.Person;
 
 public class PersonDAOImplemented implements IPersonDAO{
 
-	private IAddressDAO addressDao;
-	
-	public void setAddressDao(IAddressDAO addressDao) {
-		this.addressDao = addressDao;
-	}
-	
 	@Override
 	public Person create(Person p) {
 		// TODO Auto-generated method stub
