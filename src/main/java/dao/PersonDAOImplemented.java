3905b9f62ce78592737bca9e3298ce92a8c9c504
Chris Steele <0708104@gmail.com>
2013-05-19 00:03:14 +0200

    dependency from PersonDAO to AddressDAO exists after all... tired programming is stupid programming

--- src/main/java/dao/PersonDAOImplemented.java
+++ src/main/java/dao/PersonDAOImplemented.java
@@ -1,11 +1,18 @@
 package dao;
 
+
 import java.util.List;
 
 import domain.Person;
 
 public class PersonDAOImplemented implements IPersonDAO{
-
+	
+	private IAddressDAO addressDao;
+	
+	public void setAddressDao(IAddressDAO addressDao) {
+		this.addressDao = addressDao;
+	}
+	
 	@Override
 	public Person create(Person p) {
 		// TODO Auto-generated method stub
