3058a4654bad7512920ae6dbeb8145460b73d57f
Cornelia <e0925976@student.tuwien.ac.at>
2013-05-21 16:19:32 +0200

    gui weiter

--- src/main/java/gui/PersonOverview.java
+++ src/main/java/gui/PersonOverview.java
@@ -1,5 +1,8 @@
 package gui;
 
+import java.awt.Dimension;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import net.miginfocom.swing.MigLayout;
@@ -12,16 +15,46 @@ public class PersonOverview extends JPanel{
 	private static final long serialVersionUID = 1L;
 	private IPersonService personService;
 	private IAddressService addressService;
-
-	public PersonOverview(IPersonService personService, IAddressService addressService){
+	private Overview overview;
+	private ComponentBuilder builder;
+	private ButtonListener buttonListener;
+	private JButton create;
+	private JButton edit;
+	private JButton delete;
+	private JButton show;
+	private JButton search;
+	private JPanel panel;
+	
+	public PersonOverview(IPersonService personService, IAddressService addressService, Overview overview){
 		super(new MigLayout());
+		
+		this.overview = overview;
 		this.personService = personService;
 		this.addressService = addressService;
+		buttonListener = new ButtonListener(this);
+		builder = new ComponentBuilder();
+		panel = builder.createPanel();
+		panel.setLayout(new MigLayout());
+		panel.setPreferredSize(new Dimension(800, 800));
 		
-		setUpPersons();
+		this.add(panel);
+		
+		create = builder.createImageButton("images/template.jpg", buttonListener, "create_person");
+		panel.add(create);
+		
+		edit = builder.createImageButton("images/template.jpg", buttonListener, "edit_person");
+		panel.add(edit, "gap 25");
+		
+		delete = builder.createImageButton("images/template.jpg", buttonListener, "delete_person");
+		panel.add(delete, "gap 25");
+		
+		show = builder.createImageButton("images/template.jpg", buttonListener, "show_person");
+		panel.add(show, "gap 25");
+		
+		search = builder.createImageButton("images/template.jpg", buttonListener, "search_person");
+		panel.add(search);
+		
+		
 	}
 	
-	private void setUpPersons(){
-		
-	}
 }
