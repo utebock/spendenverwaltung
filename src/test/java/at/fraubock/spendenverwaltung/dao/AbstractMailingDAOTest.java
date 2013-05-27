8b07f9f63d9419b00b7e4e5f10d843de7f898514
Chris Steele <0708104@gmail.com>
2013-05-27 14:37:28 +0200

    Added Abstract test class for MailingDAO (tests not implemented yet)

---
+++ src/test/java/at/fraubock/spendenverwaltung/dao/AbstractMailingDAOTest.java
@@ -0,0 +1,61 @@
+package at.fraubock.spendenverwaltung.dao;
+
+import static org.junit.Assert.*;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import at.fraubock.spendenverwaltung.interfaces.domain.Mailing;
+import at.fraubock.spendenverwaltung.interfaces.domain.Address;
+import at.fraubock.spendenverwaltung.interfaces.dao.IAddressDAO;
+import at.fraubock.spendenverwaltung.interfaces.dao.IPersonDAO;
+import at.fraubock.spendenverwaltung.interfaces.dao.IMailingDAO;
+import at.fraubock.spendenverwaltung.interfaces.exceptions.PersistenceException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/testspring.xml")
+@TransactionConfiguration(defaultRollback = true)
+public class AbstractMailingDAOTest {
+
+	private static final Logger log = Logger
+			.getLogger(AbstractMailingDAOTest.class);
+
+	/**
+	 * PersonDAO is needed to create people for the tests
+	 * AddressDAO is needed to create addresses for the people used in the tests
+	 */
+	protected static IPersonDAO personDAO;
+	protected static IAddressDAO addressDAO;
+	protected static IMailingDAO mailingDAO;
+	
+	@Test(expected = IllegalArgumentException.class)
+	@Transactional
+	public void createWithNullParameter_ThrowsException() {
+		try {
+			mailingDAO.insertOrUpdate(null);
+		} catch (PersistenceException e) {
+			log.error("PersistenceException caught in test createWithNullParameter_ThrowsException");
+			fail();
+		}
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	@Transactional
+	public void createWithInvalidStateParameter_ThrowsException() {
+		try {
+			mailingDAO.insertOrUpdate(new Mailing()); // all values are null
+		} catch (PersistenceException e) {
+
+			log.error("PersistenceException caught in test createWithInvalidStateParameter_ThrowsException");
+			fail();
+		}
+	}
+	
+	
+
+}
