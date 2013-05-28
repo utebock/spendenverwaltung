9a5cc3cdd8e4350685b703ed65a1fc8d03ce23c7
Cornelia <e0925976@student.tuwien.ac.at>
2013-05-28 15:31:56 +0200

    hashcode in person hinzugefuegt

--- src/main/java/at/fraubock/spendenverwaltung/interfaces/domain/Person.java
+++ src/main/java/at/fraubock/spendenverwaltung/interfaces/domain/Person.java
@@ -204,5 +204,28 @@ public class Person {
 		return false;
 	}
 	
-	// TODO override #hashCode for hash-based data structures
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((addresses == null) ? 0 : addresses.hashCode());
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (emailNotification ? 1231 : 1237);
+		result = prime * result
+				+ ((givenName == null) ? 0 : givenName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((mainAddress == null) ? 0 : mainAddress.hashCode());
+		result = prime * result + ((note == null) ? 0 : note.hashCode());
+		result = prime * result + (postalNotification ? 1231 : 1237);
+		result = prime * result + ((sex == null) ? 0 : sex.hashCode());
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		result = prime * result
+				+ ((telephone == null) ? 0 : telephone.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
 }
