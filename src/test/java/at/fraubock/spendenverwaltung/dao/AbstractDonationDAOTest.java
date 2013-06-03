df87458ecdc3e6140ab7b68ba08cc9c57b7f1e07
Thomas <e1128165@student.tuwien.ac.at>
2013-06-04 01:18:14 +0200

    Donation Tests

--- src/test/java/at/fraubock/spendenverwaltung/dao/AbstractDonationDAOTest.java
+++ src/test/java/at/fraubock/spendenverwaltung/dao/AbstractDonationDAOTest.java
@@ -354,11 +354,31 @@ public abstract class AbstractDonationDAOTest {
 
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	@Transactional(readOnly = true)
+	public void getByPersonWithNullThrowsException() {
+		try {
+			donationDAO.getByPerson(null);
+		} catch (PersistenceException e) {
+			fail();
+		}
+	}
+
 	@Test
 	@Transactional(readOnly = true)
 	public void getWithInvalidIdReturnsNull() {
 		try {
 			assertNull(donationDAO.getByID(999999));
+		} catch (PersistenceException e) {
+			fail();
+		}
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	@Transactional(readOnly = true)
+	public void getWithNegativeIdThrowsException() {
+		try {
+			assertNull(donationDAO.getByID(-1));
 		} catch (PersistenceException e) {
 			fail();
 		}
