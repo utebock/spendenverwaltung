ca32fb6bfea96e165f1c3a596561b40c392a24eb
Philipp Muhoray <e0967964@student.tuwien.ac.at>
2013-06-03 18:59:34 +0200

    für conny

--- src/main/java/at/fraubock/spendenverwaltung/gui/filter/MountedFilterConfigurator.java
+++ src/main/java/at/fraubock/spendenverwaltung/gui/filter/MountedFilterConfigurator.java
@@ -1,11 +1,20 @@
 package at.fraubock.spendenverwaltung.gui.filter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 import at.fraubock.spendenverwaltung.gui.InvalidInputException;
+import at.fraubock.spendenverwaltung.gui.SimpleComboBoxModel;
 import at.fraubock.spendenverwaltung.gui.filter.comparators.IMountedFilterComponent;
 import at.fraubock.spendenverwaltung.interfaces.domain.filter.Filter;
+import at.fraubock.spendenverwaltung.interfaces.exceptions.ServiceException;
+import at.fraubock.spendenverwaltung.interfaces.service.IFilterService;
 import at.fraubock.spendenverwaltung.service.to.CriterionTO;
+import at.fraubock.spendenverwaltung.util.FilterType;
 
 /**
  * this component represents a specific mountable filter in the filter gui
@@ -13,20 +22,33 @@ import at.fraubock.spendenverwaltung.service.to.CriterionTO;
  * @author philipp muhoray
  * 
  */
-public class MountedFilterConfigurator implements ICriterionConfigurator {
+public class MountedFilterConfigurator extends JPanel implements
+		ICriterionConfigurator {
 
-	private Filter filter;
+	private FilterType type;
 	private IMountedFilterComponent comparator;
+	private JComboBox<Filter> filterBox;
+	private IFilterService filterService;
 
-	public MountedFilterConfigurator(Filter filter,
-			IMountedFilterComponent comparator) {
-		this.filter = filter;
+	public MountedFilterConfigurator(FilterType type,
+			IMountedFilterComponent comparator, IFilterService filterService) {
+		this.filterService = filterService;
+		this.type = type;
 		this.comparator = comparator;
+		List<Filter> filters = new ArrayList<Filter>();
+		try {
+			filters = filterService.getAll(type);
+		} catch (ServiceException e) {
+			// TODO err msg + log
+		}
+
+		add(filterBox = new JComboBox<Filter>(new SimpleComboBoxModel<Filter>(
+				filters)));
 	}
 
 	@Override
 	public CriterionTO createCriterion() throws InvalidInputException {
-		return comparator.getMountedFilterCriterionTOForFilter(filter);
+		return null;//comparator.getMountedFilterCriterionTOForFilter(filter);
 	}
 
 	@Override
