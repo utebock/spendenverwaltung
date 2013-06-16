969c25ba63a67745899379a34958476b39d9d779
Manuel Bichler <manuel.bichler@tuwien.ac.at>
2013-06-16 16:09:17 +0200

    Statistic util classes: Documentation Changed StatisticClass to not anymore save each single donation, rather saves only statistical values now.

--- src/main/java/at/fraubock/spendenverwaltung/util/statistics/Classification.java
+++ src/main/java/at/fraubock/spendenverwaltung/util/statistics/Classification.java
@@ -1,5 +1,12 @@
 package at.fraubock.spendenverwaltung.util.statistics;
 
+/**
+ * Various classification methods for plotting donation developments. Defines
+ * the size of "buckets" on the time axis.
+ * 
+ * @author manuel-bichler
+ * 
+ */
 public enum Classification {
 	DAY, WEEK, MONTH, QUARTER, YEAR;
 }
