589decad4bc85174e6ca86cde23f185015114372
Philipp Muhoray <e0967964@student.tuwien.ac.at>
2013-06-23 21:26:20 +0200

    old gui files deleted

--- src/main/java/at/fraubock/spendenverwaltung/gui/App.java
+++
@@ -1,9 +0,0 @@
-package at.fraubock.spendenverwaltung.gui;
-
-@Deprecated
-public class App {
-	
-    public static void main( String[] args )   {
-		new StartMainFrame().startMainFrame();
-    }
-}
