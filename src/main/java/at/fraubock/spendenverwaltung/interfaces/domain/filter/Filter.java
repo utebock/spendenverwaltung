d01da1e44728d020cca479e360aca804119b0a9e
Günter Dressel <g.dressel@cyledge.com>
2013-12-16 15:32:26 +0100

    added GPLv3 licence information

--- src/main/java/at/fraubock/spendenverwaltung/interfaces/domain/filter/Filter.java
+++ src/main/java/at/fraubock/spendenverwaltung/interfaces/domain/filter/Filter.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Bockmas TU Vienna Team.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ * Bockmas TU Vienna Team: Cornelia Hasil, Chris Steele, Manuel Bichler, Philipp Muhoray, Roman Voglhuber, Thomas Niedermaier
+ * 
+ * http://www.fraubock.at/
+ ******************************************************************************/
 package at.fraubock.spendenverwaltung.interfaces.domain.filter;
 
 import at.fraubock.spendenverwaltung.interfaces.domain.filter.criterion.Criterion;
